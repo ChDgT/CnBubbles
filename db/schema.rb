691863de0135da2379a66750a8b929ea4dd5cc4a
charles degrassat <charles.degrassat@gmail.com>
2019-05-23 15:43:54 +0200

    Create models done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_132137) do
+ActiveRecord::Schema.define(version: 2019_05_23_133940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "comic_id"
+    t.boolean "available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comic_id"], name: "index_bookings_on_comic_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "comics", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "category"
+    t.date "publication_date"
+    t.string "photo"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comics_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +49,7 @@ ActiveRecord::Schema.define(version: 2019_05_23_132137) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "comics"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "comics", "users"
 end
