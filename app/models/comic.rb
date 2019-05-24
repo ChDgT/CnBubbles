1907fd97777ed65e6ce2036daf201378e7721a8c
charles degrassat <charles.degrassat@gmail.com>
2019-05-24 18:19:58 +0200

    evening

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -4,4 +4,6 @@ class Comic < ApplicationRecord
   validates :description, presence: true, allow_blank: false
   validates :category, presence: true, allow_blank: false
   mount_uploader :photo, PhotoUploader
+  validates :publication_date, inclusion: {in: (1899..Time.now.to_a[5]), message: "%{value} really ??"}
+
 end
