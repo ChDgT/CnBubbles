20041e8b223a5383aaa4aa26229d4d17e908ad5b
charles degrassat <charles.degrassat@gmail.com>
2019-05-24 16:58:48 +0200

    bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_134953) do
+ActiveRecord::Schema.define(version: 2019_05_24_144040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,12 +28,12 @@ ActiveRecord::Schema.define(version: 2019_05_24_134953) do
     t.string "title"
     t.text "description"
     t.string "category"
-    t.date "publication_date"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
     t.boolean "available", default: true
+    t.integer "publication_date"
     t.index ["user_id"], name: "index_comics_on_user_id"
   end
 
